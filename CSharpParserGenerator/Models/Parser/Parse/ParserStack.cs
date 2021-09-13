5828b2490aa46fa75f8ddcf65247af689025fc7e
Rolando A. Rosales J <rolando.develop@gmail.com>
2021-09-12 21:07:48 -0500

    ParserStack debugger display

--- CSharpParserGenerator/Models/Parser/Parse/ParserStack.cs
+++ CSharpParserGenerator/Models/Parser/Parse/ParserStack.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Id = System.Int64;
 
 namespace CSharpParserGenerator
 {
+
+    [DebuggerDisplay("Count = {Values?.Count}")]
+    [DebuggerTypeProxy(typeof(ParserStackDebugView))]
     public class ParserStack
     {
         private int PointerIdx { get; set; }
@@ -66,6 +70,50 @@ namespace CSharpParserGenerator
         public void Goto<ELang>(ActionState<ELang> action) where ELang : Enum
         {
             States[States.Count - 1] = action.To;
+        }
+
+        // Debug View
+        internal class ParserStackDebugView
+        {
+            private ParserStack ParserStack;
+            public ParserStackDebugView(ParserStack parserStack)
+            {
+                ParserStack = parserStack;
+            }
+
+            [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+            public StackItem[] items
+            {
+                get
+                {
+                    if (ParserStack.Values == null) return null;
+
+                    var values = ParserStack.Values;
+                    var pointerIdx = ParserStack.PointerIdx;
+                    StackItem[] items = new StackItem[values.Count];
+
+                    int i = 0;
+                    foreach (object value in values)
+                    {
+                        items[i] = new StackItem(i - pointerIdx, value);
+                        i++;
+                    }
+                    return items;
+                }
+            }
+        }
+
+        [DebuggerDisplay("{Value}", Name = "{Idx}")]
+        internal class StackItem
+        {
+            private int Idx;
+            private object Value;
+
+            public StackItem(int idx, object value)
+            {
+                this.Idx = idx;
+                this.Value = value;
+            }
         }
     }
 }
